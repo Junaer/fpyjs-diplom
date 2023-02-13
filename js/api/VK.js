450468f3f074ef6fc1888259d546805c0c62ab6f
Мызников Евгений Игоревич <myznikovei@admlr.lipetsk.ru>
2023-02-13 17:21:23 +0300

    end project 3

--- js/api/VK.js
+++ js/api/VK.js
@@ -41,15 +41,3 @@ class VK {
     this.lastCallback = () => {};
   };
 }
-
-// https://oauth.vk.com/blank.html#access_token=vk1.a.K4nmk7vrdTf2nXobLsBOhD-p4B8nqePS-86gNVFBcCGYcnxtHc3bpSGJLdhFv1L9GmlgUZivC9QyftYqCdFC9cQFX_kppBoY42YuTFf_cl8tO80ZKAL1OnCMK4O0ruOJ-L6t0ChEVJlXfmE7e5S-uAlzn3Dv3-Jan6XF6LAILADA0iMOe9KivQHKVxOdI9XQPsPk06OyFXmT0Or7tjdlgA&expires_in=86400&user_id=737580348&state=123456
-// vk1.a.K4nmk7vrdTf2nXobLsBOhD-p4B8nqePS-86gNVFBcCGYcnxtHc3bpSGJLdhFv1L9GmlgUZivC9QyftYqCdFC9cQFX_kppBoY42YuTFf_cl8tO80ZKAL1OnCMK4O0ruOJ-L6t0ChEVJlXfmE7e5S-uAlzn3Dv3-Jan6XF6LAILADA0iMOe9KivQHKVxOdI9XQPsPk06OyFXmT0Or7tjdlgA
-// Это идентификатор вашего приложения. Используйте его в запросах для получения OAuth-токена
-// dc5649ea0e4b468893939b35aa7eee12
-
-// Секретный ключ, которым будет подписан jwt-токен с информацией о пользователе
-// f6024a0332f544fe80a0c5ab11cd6f76
-
-
-
-
